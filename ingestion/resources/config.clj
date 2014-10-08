61b5694a3d0f92841d016870c696658511d3a9ef
Connor Jennings <cojennin@gmail.com>
2014-10-08 00:08:09 -0400

    This will correctly parse non-nested maps.

--- ingestion/resources/config.clj
+++ ingestion/resources/config.clj
@@ -7,12 +7,12 @@
                   :location "https://spreadsheets.google.com/feeds/list/0AgT-U5vP_iLMdFBITF9ieE1UaGNHX0dvWlR1aThSV1E/od6/public/values?alt=json",
                   :name "distilleries"
                   :mapping { :name "gsx$distilleryname.$t", :state "gsx$state.$t" }
-                },
-               {
-                  :location "https://spreadsheets.google.com/feeds/list/0AgT-U5vP_iLMdFBITF9ieE1UaGNHX0dvWlR1aThSV1E/od6/public/values?alt=json",
-                  :name "spirits"
-                  :mapping { :name "gsx$brandname.$t", :class "gsx$classofspirit.$t", :certification "gsx$certification.$t", :distillery { :name "gsx$distilleryname.$t", :state "gsx$state.$t" } }
-                 }
+                }
+;               {
+;                  :location "https://spreadsheets.google.com/feeds/list/0AgT-U5vP_iLMdFBITF9ieE1UaGNHX0dvWlR1aThSV1E/od6/public/values?alt=json",
+;                  :name "spirits"
+;                  :mapping { :name "gsx$brandname.$t", :class "gsx$classofspirit.$t", :certification "gsx$certification.$t", :distillery { :name "gsx$distilleryname.$t", :state "gsx$state.$t" } }
+;                 }
 
                ]
   }
--- ingestion/src/ingestion/core.clj
+++ ingestion/src/ingestion/core.clj
@@ -14,37 +14,51 @@
 (defn parse-json [data]
   (parse-string data true))
 
-(defn get-spreadsheet-entry-value-from-dsl [dsl entry]
-  (do
-    (let [props (str/split dsl #"\.")]
-      (loop [list-key props
-             map-val entry]
-        (if (= (count props) 1)
-          ((keyword (first props)) map-val)
-          (do
-            (recur (next props) ((keyword (first props)) entry))))))))
+;(defn get-spreadsheet-entry-value-from-dsl [dsl entry]
+;  (do
+;    (let [props (str/split dsl #"\.")]
+;      (loop [list-key props
+;             map-val entry]
+;        (if (nil? props)
+;          ((keyword (first props)) map-val)
+;          (do
+;            (recur (next props) ((keyword (first props)) entry))))))))
+;
+;(defn map-props-from-spreadsheet [properties data]
+;  (loop [finished-map {}
+;         props properties]
+;    (let [prop-key (key (first props))
+;          prop-val (val (first props))]
+;         (if (empty? props)
+;           finished-map
+;           (if (map? prop-val)
+;               (recur (assoc finished-map prop-key (recur {} prop-val)) (next props))
+;               (recur (assoc finished-map prop-key (get-spreadsheet-entry-value-from-dsl prop-val data)) (next props)))))))
 
-(defn map-props-from-spreadsheet [properties data]
-  (loop [finished-map {}
-         props properties]
-    (let [prop-key (key (first props))
-          prop-val (val (first props))]
-         (if (empty? props)
-           finished-map
-           (if (map? prop-val)
-             (recur (assoc finished-map prop-key prop-val) prop-val)
-             (recur (assoc finished-map prop-key (get-spreadsheet-entry-value-from-dsl prop-val data)) (next props)))))))
+(defn get-data-from-dsl [dsl data]
+  (let [props (str/split dsl #"\.")]
+    (loop [[prop & rest-of-props] props
+           val data]
+      (if (nil? prop)
+        val
+        (do
+          (recur rest-of-props ((keyword prop) val)))))))
+
+(defn map-to-data [the-map]
+  (let [props-to-map the-map]
+    (fn [object-to-extract-values-from]
+        (loop [properties props-to-map
+               finished-mapping {}]
+          (let [key (first (keys properties))
+                val (first (vals properties))]
+            (if (empty? properties)
+              finished-mapping
+              (recur (next properties) (assoc finished-mapping key (get-data-from-dsl val object-to-extract-values-from)))))))))
 
 ; Takes a goole spreadsheet as json
 (defn process-google-spreadsheet [spreadsheet mapping]
-  (let [obj (:entry (:feed (parse-json spreadsheet)))]
-    (loop [remaining-entries obj
-           mapped-entries []]
-      (if (empty? remaining-entries)
-        mapped-entries
-        (let [[is-mapping & to-map] remaining-entries
-              mapped-entries (conj mapped-entries (map-props-from-spreadsheet mapping is-mapping))]
-          (recur to-map mapped-entries))))))
+  (let [objs (:entry (:feed (parse-json spreadsheet)))]
+    (map (map-to-data mapping) objs)))
 
 (defn -main
   "I don't do a whole lot ... yet."
