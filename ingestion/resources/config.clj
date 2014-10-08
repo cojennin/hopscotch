77a78511317a4d12f3516636c719a9cfd20468aa
Connor Jennings <cojennin@gmail.com>
2014-10-08 00:16:44 -0400

    Hey, we got nested mapping.

--- ingestion/resources/config.clj
+++ ingestion/resources/config.clj
@@ -3,12 +3,11 @@
               :host "127.0.0.1"
               :database "ingestion"
               }
-  :resources [
-;               {
-;                  :location "https://spreadsheets.google.com/feeds/list/0AgT-U5vP_iLMdFBITF9ieE1UaGNHX0dvWlR1aThSV1E/od6/public/values?alt=json",
-;                  :name "distilleries"
-;                  :mapping { :name "gsx$distilleryname.$t", :state "gsx$state.$t" }
-;                },
+  :resources [{
+                  :location "https://spreadsheets.google.com/feeds/list/0AgT-U5vP_iLMdFBITF9ieE1UaGNHX0dvWlR1aThSV1E/od6/public/values?alt=json",
+                  :name "distilleries"
+                  :mapping { :name "gsx$distilleryname.$t", :state "gsx$state.$t" }
+                },
                {
                   :location "https://spreadsheets.google.com/feeds/list/0AgT-U5vP_iLMdFBITF9ieE1UaGNHX0dvWlR1aThSV1E/od6/public/values?alt=json",
                   :name "spirits"
